f959d7b14473b781c38672c806062c76e4e221f8
SvetaDan <sveta.danielyan.99@gmail.com>
2021-06-20 03:49:29 +0400

    gitignore should work now

---
+++ MultipleSortingAlgorithms/MergeSort.cs
@@ -0,0 +1,71 @@
+﻿namespace MultipleSortingAlgorithms
+{
+    public class MergeSort : ISort
+    {
+        public void Sort(int[] array)
+        {
+            SortMergesArrays(array, 0, array.Length - 1);
+        }
+
+        void Merge(int[] array, int l, int m, int r)
+        {
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            int i, j;
+
+            for (i = 0; i < n1; ++i)
+                L[i] = array[l + i];
+            for (j = 0; j < n2; ++j)
+                R[j] = array[m + 1 + j];
+
+            i = 0;
+            j = 0;
+
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    array[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    array[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < n1)
+            {
+                array[k] = L[i];
+                i++;
+                k++;
+            }
+
+            while (j < n2)
+            {
+                array[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+
+        void SortMergesArrays(int[] array, int l, int r)
+        {
+            if (l < r)
+            {
+                int m = l + (r - l) / 2;
+                SortMergesArrays(array, l, m);
+                SortMergesArrays(array, m + 1, r);
+
+                Merge(array, l, m, r);
+            }
+        }
+
+    }
+}
---
+++ MultipleSortingAlgorithms/SortHelper.cs
@@ -0,0 +1,33 @@
+﻿namespace MultipleSortingAlgorithms
+{
+    public class SortHelper
+    {
+        public static void SortByStrategy(SortStrategy sortMethod, int[] array)
+        {
+            if (array.Length == 0) { return; }
+
+            SortHandler sortHandler = null;
+            switch (sortMethod)
+            {
+                case SortStrategy.Insertion:
+                    InsertionSort insertionSort = new InsertionSort();
+                    sortHandler = new SortHandler(insertionSort, array);
+                    break;
+
+                case SortStrategy.Selection:
+                    SelectionSort selectionSort = new SelectionSort();
+                    sortHandler = new SortHandler(selectionSort, array);
+                    break;
+
+                case SortStrategy.Merge:
+                    MergeSort mergeSort = new MergeSort();
+                    sortHandler = new SortHandler(mergeSort, array);
+                    break;
+            }
+
+            if (sortHandler == null) { return; }
+            sortHandler.SortArray();
+            sortHandler.ShowSortedArray();
+        }
+    }
+}
